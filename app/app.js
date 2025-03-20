f1544ec131ac516235ea2b34b817809690ae873f
ruthwikSai <ruthwiksaidebbata@gmail.com>
2025-03-20 20:04:44 +0000

    pug configuration

--- app/app.js
+++ app/app.js
@@ -6,6 +6,9 @@ var app = express();
 
 // Add static files location
 app.use(express.static("static"));
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
